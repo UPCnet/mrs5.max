7a1367ece32e8ca04e7bc0c391b6c107d17a3881
Pilar Marinas <pilarmarinas@MBP-de-Pilar.local.lan>
2017-06-23 11:28:05 +0200

    Comentar override para que no pete Pdte

--- src/mrs5/max/userdataschema.py
+++ src/mrs5/max/userdataschema.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
 from zope import schema
 from zope.interface import implements
-from plone.app.users.schema import IUserDataSchema
+from plone.app.users.schema import IUserDataSchema, ICombinedRegisterSchema
 from plone.app.users.schema import IUserSchemaProvider
 from plone.app.users.browser.userdatapanel import UserDataPanelAdapter
+from plone.app.users.browser.userdatapanel import UserDataPanel
 
 from ZPublisher.HTTPRequest import FileUpload
 from Products.CMFPlone import PloneMessageFactory as PLMF
+from zope.component.hooks import getSite
+from plone.app.users.browser.schemaeditor import getFromBaseSchema
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 from mrs5.max import MRSMAXMessageFactory as _
 
 
-class IEnhancedUserDataSchema(IUserDataSchema):
+class IEnhancedUserDataSchema(ICombinedRegisterSchema):
     """ Use all the fields from the default user data schema, and add various
     extra fields.
     """
@@ -33,19 +38,34 @@ class IEnhancedUserDataSchema(IUserDataSchema):
     )
 
 
-class UserDataSchemaProvider(object):
-    implements(IUserSchemaProvider)
+# class UserDataSchemaProvider(UserDataPanel):
+    
+#     @property
+#     def schema(self):
+#         portal = getSite()
+#         schema = getattr(portal, '_v_userdata_schema', None)
+#         if schema is None:
+#             portal._v_userdata_schema = schema = getFromBaseSchema(
+#                 IEnhancedUserDataSchema,
+#                 form_name=u'In User Profile'
+#             )
+#             # as schema is a generated supermodel,
+#             # needed adapters can only be registered at run time
+#             provideAdapter(EnhancedUserDataPanelAdapter, (IPloneSiteRoot,), schema)
+#         return schema
 
-    def getSchema(self):
-        """
-        """
-        return IEnhancedUserDataSchema
+    # def getSchema(self):
+    #     """
+    #     """
+    #     return IEnhancedUserDataSchema
 
 
 class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
+    
     def get_twitter_username(self):
         return self.context.getProperty('twitter_username', '')
 
     def set_twitter_username(self, value):
         return self.context.setMemberProperties({'twitter_username': value})
+    
     twitter_username = property(get_twitter_username, set_twitter_username)
