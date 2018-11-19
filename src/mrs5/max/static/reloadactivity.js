a383997434d3ffd373dded9c34ab59cb43480811
Iago López Fernández <iago.lopez@upcnet.es>
2018-11-19 12:04:49 +0100

    reloadactivity.js - Lanzar solo si esta activado la tab de Actividad

--- src/mrs5/max/static/reloadactivity.js
+++ src/mrs5/max/static/reloadactivity.js
@@ -1,15 +1,27 @@
-msgCA = max.literals('ca')['new_comment_text'];
-msgES = max.literals('es')['new_comment_text'];
-msgEN = max.literals('en')['new_comment_text'];
-msgCommentBox = [msgCA, msgES, msgEN]
-setInterval(function(){
-  var notWrite = true;
-  $(".maxui-newcommentbox textarea").each(function(){
-    if($(this).val() != "" && !msgCommentBox.includes($(this).val())){
-      notWrite = false;
-    }
-  });
-  if(notWrite){
-    $("#maxui-news-activities .maxui-button").trigger("click");
-  }
-}, 180000);
+var reloadActivity = function(){
+    msgCA = max.literals('ca')['new_comment_text'];
+    msgES = max.literals('es')['new_comment_text'];
+    msgEN = max.literals('en')['new_comment_text'];
+    msgCommentBox = [msgCA, msgES, msgEN]
+    setInterval(function(){
+      var notWrite = true;
+      $(".maxui-newcommentbox textarea").each(function(){
+        if($(this).val() != "" && !msgCommentBox.includes($(this).val())){
+          notWrite = false;
+        }
+      });
+      if(notWrite){
+        $("#maxui-news-activities .maxui-button").trigger("click");
+      }
+    }, 180000);
+};
+
+$('#menusup > nav > div:first-of-type').click(reloadActivity);
+
+$(document).ready(function(){
+    setTimeout(function(){
+        if($('#menusup > nav > div:first-of-type').hasClass('active')){
+            reloadActivity();
+        }
+    }, 500);
+});
